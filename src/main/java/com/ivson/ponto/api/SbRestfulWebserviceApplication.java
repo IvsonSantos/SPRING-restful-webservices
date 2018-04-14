e8bb950c711bbe329616d6cf334055bc884f2062
Ivson Santos <santosivson@hotmail.com>
2018-04-14 16:42:52 -0300

    first commit

---
+++ src/main/java/com/ivson/ponto/api/SbRestfulWebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.ivson.ponto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestfulWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestfulWebserviceApplication.class, args);
+	}
+}
